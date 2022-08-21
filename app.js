dc46e604a84f2db5687c3b696eb74fc2b9648bd0
Dusan <dusanvulic11@gmail.com>
2022-08-21 12:43:50 +0200

    get single document

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 
 const { connectDb, getDb } = require("./db");
 
+//import from mongoDB in order to read id
+const { ObjectId } = require("mongodb");
+
 //init app and middleware
 
 const app = express();
@@ -37,4 +40,19 @@ app.get("/books", (req, res) => {
         });
 
     // res.json({ msg: "welcome to the api" });
+});
+
+app.get("/books/:id", (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection("books")
+            .findOne({ _id: ObjectId(req.params.id) })
+            .then((doc) => {
+                return res.status(200).json(doc);
+            })
+            .catch((error) => {
+                res.status(500).json({ error: "there is no book with that ID" });
+            });
+    } else {
+        res.status(500).json({ error: "not a vali id document" });
+    }
 });
