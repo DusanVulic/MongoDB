96ee53735df97e537e75465a971811bb0b7c810c
Dusan <dusanvulic11@gmail.com>
2022-08-21 11:27:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+const express = require("express");
+
+//init app and middleware
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("app listening on port 3000");
+});
+
+/// routes
+
+app.get("/books", (req, res) => {
+    res.json({ msg: "welcome to the api" });
+});
